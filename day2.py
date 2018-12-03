aa9700a5cd582b8fa51e5703ddee0e02951f495d
Ry <ry.e.desoto@gmail.com>
2018-12-02 22:03:08 -0600

    added day2 part2

--- day2.py
+++ day2.py
@@ -29,3 +29,45 @@ def getCheckSum(arr):
 
 # calculate
 print(getCheckSum(alist))
+
+# part 2:
+# note: i know this isn't very pythonic. if i wanted to get pythonic with it, i'd use itertools, but i wanted 
+# to do this without additional modules. this was a dumb thing to do, but i did it :')
+
+def getSame(arr):
+    
+    morediff = {} #for testing purposes
+
+    #using indices for the list in order to compare each value to the values following
+    for i in range(len(arr)):
+        for j in range(i+1, len(arr)):
+            firstdiff = False
+            seconddiff = False
+
+            #using the index values for each letter, compare each letter to its equivalent in the next items
+            for k in range(len(arr[i])):
+                if arr[i][k] != arr[j][k] and firstdiff == False:
+                    firstdiff = True
+                
+                elif arr[i][k] != arr[j][k]:
+                    seconddiff = True   
+
+            if seconddiff == True:
+                morediff.update({arr[i] : arr[j]}) #for testing purposes
+            else:
+                return arr[i], arr[j]
+    
+# compare each item in the 2-part tuple to get the common letters
+def compareTup(tup):
+    s1 = tup[0]
+    s2 = tup[1]
+    common = []
+    
+    for i in range(len(s1)):
+        if s1[i] == s2[i]:
+            common.append(s1[i])
+    
+    return ''.join(common)
+
+sameGet = getSame(alist)
+print(compareTup(sameGet))
